9a848e8b47a66764af8813d220262741a5b85026
aroviqen <aroviqen@secpal.app>
2026-07-18 09:15:45 +0000

    Align development script license

--- scripts/dev.php
+++ scripts/dev.php
@@ -3,7 +3,7 @@
 
 /*
  * SPDX-FileCopyrightText: 2026 SecPal Contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later AND LicenseRef-SecPal-Attribution
+ * SPDX-License-Identifier: MIT
  */
 
 declare(strict_types=1);
